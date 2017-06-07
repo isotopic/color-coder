c0a52fe2a1ef19461f20dc8122567a0db764ab16
Guilherme Cruz <cin_ccruz@uolinc.com>
2017-06-07 19:32:52 -0300

    Service worker

---
+++ sw.js
@@ -0,0 +1,38 @@
+
+var CACHE_NAME = 'v1';
+
+var urlsToCache = [
+	'index.html',
+	'css/app.css',
+	'css/font.css',
+	'js/color-coder.js',
+	'sound/yes.mp3',
+	'sound/no.mp3',
+	'phonegap.js'
+];
+
+this.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open('v1').then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+
