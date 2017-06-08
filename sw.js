2cf2511c2438e2e40213e5ff2bc18622f1bae648
Guilherme <guilhermecruz@gmail.com>
2017-06-07 21:35:42 -0300

    fix link again

--- sw.js
+++ sw.js
@@ -19,12 +19,10 @@ this.addEventListener('install', function(event) {
 	);
 });
 
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        // Cache hit - return response
         if (response) {
           return response;
         }
